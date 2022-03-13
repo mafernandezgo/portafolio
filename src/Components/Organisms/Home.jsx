cb288ec2a9832a693823c961414839c853c1e6c0
Manuela <manue93_55@hotmail.com>
2022-03-13 01:14:56 -0500

    fixing Home

--- src/Components/Organisms/Home.jsx
+++ src/Components/Organisms/Home.jsx
@@ -1,10 +1,18 @@
 import './home.css'
 import test from './test.png'
 function Home (){
+
+    let name = "Manuela Fernandez"
+//     let nameSplit = name.split('')
+
+//     function letterDancing () {  return nameSplit.forEach(x => 
+//         x.addEventListener("click", () => {console.log(x)}))
+// }
+
     return(
         <section id="home" className="home">
             <div className="imgWrapper"><img className="img" src={test}/></div>
-            <h1> Hello World, <br/> I'm <span className="name">Manuela Fernandez</span> <br/> Frontend Developer</h1>
+            <h1> Hello World, <br/> I'm <span className="name">{name}</span> <br/> Frontend Developer</h1>
         </section>
     )
 }
